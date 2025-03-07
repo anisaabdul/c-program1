555024b53129bf607bd13a9c3825a5a1d6cdf878
anisaabdul <anisamehfil@gmail.com>
2025-03-07 10:35:13 +0530

    Add files via upload

---
+++ Anisa.c/program1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+ double x,y,z,w,result;
+ printf("enter a value for x:");
+ scanf("%lf",&x);
+ printf("enter a value for y:");
+ scanf("%lf",&y);
+ printf("enter avalue for w:");
+ scanf("%lf",&w);
+ result=(pow(x+y,2)*(x+z))/w;
+ printf("the result is: %.2lf \n",result);
+ return 0;
+}
---
+++ Anisa.c/program4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ float basic,bonus,commision,totSales,totSalary;
+ int itemsSold;
+ printf("enter the basic salary:");
+ scanf("%f",&basic);
+ printf("enter the bonus per item sold:");
+ scanf("%f"<&bonus):
+ printf("enter the commision percentage:");
+ scanf("%f",&commision);
+ printf("enter the number of items sold:");
+ scanf("%d",&itemsSold);
+ printf("enter the total monthly sales:");
+ scanf("%f",&totalSales);
+ bonus= itemsSold*bonus
+ commision=(commision/100)*TOTALsALES;
+ printf("the total salary of the salesmanis: %.2f\n",totalSalary);
+ return 0;
+}
